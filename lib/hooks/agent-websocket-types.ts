080a07531e6674594c2c26723f1847e4c7960690
prophet_system_team <vivekvjnk@gmail.com>
2026-02-08 10:38:30 +0530

    runframe is working in synchrony with relay server an agent

--- lib/hooks/agent-websocket-types.ts
+++ lib/hooks/agent-websocket-types.ts
@@ -44,7 +44,7 @@ export interface AgentMessage {
     type: EventType
     artifact_id: string | null
     timestamp: string // ISO-8601
-    source: "runtime" | "backend"
+    source: "runtime" | "backend" 
     payload: any
 }
 
